24321d696692dc08ace431e26f46d5eea7245e04
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-02 17:34:11 +0100

    Updates

---
+++ precache-manifest.a5d4cd40d1392f4fda336117e35bb58c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de0984188b30a9dd4d8d7690fcf50d9",
+    "url": "/wacho-bs-react/index.html"
+  },
+  {
+    "revision": "665b16581742b2a08d41",
+    "url": "/wacho-bs-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a94959ed9c541918018a",
+    "url": "/wacho-bs-react/static/js/2.f3011e29.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wacho-bs-react/static/js/2.f3011e29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "665b16581742b2a08d41",
+    "url": "/wacho-bs-react/static/js/main.6e196d1e.chunk.js"
+  },
+  {
+    "revision": "3b8f7b08cf7c59ff240e",
+    "url": "/wacho-bs-react/static/js/runtime-main.79b3f8fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wacho-bs-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
