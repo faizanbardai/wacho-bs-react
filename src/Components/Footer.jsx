e52f38e9c7499d32ee0e209845ce3a97aa28c4a5
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-13 16:43:27 +0100

    hide nav on scroll

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="text-white bg-dark py-4" id="contact">
         <div className="container">
-          <div className="row">
+          <div className="row vh-100 d-flex justify-content-around align-items-center">
             <div className="col-12 col-lg-6">
               <iframe title="Google-Maps"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2663.2591076019726!2d11.58549961505705!3d48.12452636004989!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479ddf7a6ae41f0f%3A0x83fe7076b2716f86!2sRegerpl.%205%2C%2081541%20M%C3%BCnchen!5e0!3m2!1sen!2sde!4v1573644873814!5m2!1sen!2sde"
