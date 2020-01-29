507a5864cec395cf8c62765a1769121cd1788d4c
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-29 20:06:46 +0100

    moved route to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { BrowserRouter as Router } from "react-router-dom";
-import MainComponent from "./Components/MainComponent";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import MainComponent from "./Components/MainComponent";
+import Home from "./Components/Home";
+import Admin from "./Components/Admin/Admin";
 
 ReactDOM.render(
-  // <Router>
-    <MainComponent />,
-  // </Router>,
+  <Router>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+      <Route path="/en" exact>
+        <Home lang="en" />
+      </Route>
+      <Route path="/" exact>
+        <Home lang="de" />
+      </Route>
+      {/* <MainComponent /> */}
+    </Switch>
+  </Router>,
   document.getElementById("root")
 );
