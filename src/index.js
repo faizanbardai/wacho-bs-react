db9eb701769311b617b7de48633ac0353c928ea2
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-29 20:19:43 +0100

    Added HashRouter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  HashRouter,
+  Switch
+} from "react-router-dom";
 // import MainComponent from "./Components/MainComponent";
 import Home from "./Components/Home";
 import Admin from "./Components/Admin/Admin";
 
 ReactDOM.render(
   <Router>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/en" exact>
-        <Home lang="en" />
-      </Route>
-      <Route path="/" exact>
-        <Home lang="de" />
-      </Route>
-      {/* <MainComponent /> */}
-    </Switch>
+    <HashRouter basename="/">
+      <Switch>
+        <Route path="/admin" component={Admin} />
+        <Route path="/en" exact>
+          <Home lang="en" />
+        </Route>
+        <Route path="/" exact>
+          <Home lang="de" />
+        </Route>
+        {/* <MainComponent /> */}
+      </Switch>
+    </HashRouter>
   </Router>,
   document.getElementById("root")
 );
