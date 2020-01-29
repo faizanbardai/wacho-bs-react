59111addb0ebc20706fb13553fb30480c065d885
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-29 12:12:13 +0100

    revised changes

--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
-import Admin from "./Admin/Admin";
+// import Admin from "./Admin/Admin";
 
 export default class MainComponent extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <Switch>
-            <Route path="/admin" component={Admin} />
-            <Route path="/" exact>
-              <Home lang="de" />
-            </Route>
-            <Route path="/en" exact>
+        {/* <Router> */}
+          {/* <Switch> */}
+            {/* <Route path="/admin" component={Admin} /> */}
+            {/* <Route path="/" exact> */}
+              {/* <Home lang="de" /> */}
+            {/* </Route> */}
+            {/* <Route path="/en" exact> */}
               <Home lang="en" />
-            </Route>
-          </Switch>
-        </Router>
+            {/* </Route> */}
+          {/* </Switch> */}
+        {/* </Router> */}
       </div>
     );
   }
