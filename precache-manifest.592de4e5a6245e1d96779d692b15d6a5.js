2a8bc7e2e0d30efad58e9a621642e53f7af957e4
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-15 13:39:50 +0200

    Updates

---
+++ precache-manifest.592de4e5a6245e1d96779d692b15d6a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551c3f9ac0896d3be0dda88748535b44",
+    "url": "/wacho-bs-react/index.html"
+  },
+  {
+    "revision": "5180d50b7e340d99d503",
+    "url": "/wacho-bs-react/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "be6ed95d2f94e2621e31",
+    "url": "/wacho-bs-react/static/css/main.a75e03a5.chunk.css"
+  },
+  {
+    "revision": "5180d50b7e340d99d503",
+    "url": "/wacho-bs-react/static/js/2.bd18b57e.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/wacho-bs-react/static/js/2.bd18b57e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be6ed95d2f94e2621e31",
+    "url": "/wacho-bs-react/static/js/main.76334578.chunk.js"
+  },
+  {
+    "revision": "3b8f7b08cf7c59ff240e",
+    "url": "/wacho-bs-react/static/js/runtime-main.79b3f8fc.js"
+  },
+  {
+    "revision": "09f4b4ecf124c13d1e8bd94f455f0aa3",
+    "url": "/wacho-bs-react/static/media/aboutMeImg.09f4b4ec.jpg"
+  },
+  {
+    "revision": "f3bd35847b680f75e6e308f8096a83f2",
+    "url": "/wacho-bs-react/static/media/header-loop.f3bd3584.mp4"
+  },
+  {
+    "revision": "ad029bf895494ee7d43899ec09dac2c0",
+    "url": "/wacho-bs-react/static/media/myDreamImg.ad029bf8.jpg"
+  },
+  {
+    "revision": "fa68be640195acd26efce34234a87f34",
+    "url": "/wacho-bs-react/static/media/philosophy.fa68be64.jpg"
+  },
+  {
+    "revision": "d71a8e1e1199d7376392aa00e12a104e",
+    "url": "/wacho-bs-react/static/media/sociology.d71a8e1e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03dfdf70b02d015d0e6827f02146230b.js"
+  "/wacho-bs-react/precache-manifest.592de4e5a6245e1d96779d692b15d6a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wacho-bs-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
