2022db70cbe8ca27fb86b27ee9527b2a4985998f
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-16 11:04:54 +0200

    Updates

---
+++ precache-manifest.e02f24fd3cb084d7a1afd52f70a9df03.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e537a84b199b098dacb2f78088d6f626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2460d1bc134c5594b454",
+    "url": "/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "7d361bfff4ff5ac5124a",
+    "url": "/static/css/main.a75e03a5.chunk.css"
+  },
+  {
+    "revision": "2460d1bc134c5594b454",
+    "url": "/static/js/2.1e07bf34.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/static/js/2.1e07bf34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d361bfff4ff5ac5124a",
+    "url": "/static/js/main.ecab7e28.chunk.js"
+  },
+  {
+    "revision": "6b3492426a061986161d",
+    "url": "/static/js/runtime-main.5fe3c217.js"
+  },
+  {
+    "revision": "09f4b4ecf124c13d1e8bd94f455f0aa3",
+    "url": "/static/media/aboutMeImg.09f4b4ec.jpg"
+  },
+  {
+    "revision": "f3bd35847b680f75e6e308f8096a83f2",
+    "url": "/static/media/header-loop.f3bd3584.mp4"
+  },
+  {
+    "revision": "ad029bf895494ee7d43899ec09dac2c0",
+    "url": "/static/media/myDreamImg.ad029bf8.jpg"
+  },
+  {
+    "revision": "fa68be640195acd26efce34234a87f34",
+    "url": "/static/media/philosophy.fa68be64.jpg"
+  },
+  {
+    "revision": "d71a8e1e1199d7376392aa00e12a104e",
+    "url": "/static/media/sociology.d71a8e1e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "goldenesvliesmmm.de/precache-manifest.0f6c1f3e1a70726e29a5b6ef21c15887.js"
+  "/precache-manifest.e02f24fd3cb084d7a1afd52f70a9df03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("goldenesvliesmmm.de/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
