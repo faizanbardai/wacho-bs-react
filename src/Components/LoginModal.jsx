7dcb645a15f45c0fa1ded77553d5e8a8781b8ee5
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-17 13:48:25 +0200

    Admin Login section

--- src/Components/LoginModal.jsx
+++ src/Components/LoginModal.jsx
@@ -1,36 +1,54 @@
 import React, { useState } from "react";
-import { Button, Container } from "react-bootstrap";
-import { Modal } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 export default function LoginModal() {
-  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   return (
     <>
-      <Container className="">
+      <Container>
         <div className="vh-100 d-flex justify-content-center align-items-center">
-          <Button variant="primary" onClick={handleShow}>
-            Login
-          </Button>
+          <form
+            style={{ minWidth: "300px" }}
+            className="form-signin"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="inputEmail" className="sr-only">
+              Email address
+            </label>
+            <input
+              type="email"
+              id="inputEmail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mb-3 form-control"
+              placeholder="Email address"
+              required
+              autoFocus
+            />
+            <label htmlFor="inputPassword" className="sr-only">
+              Password
+            </label>
+            <input
+              type="password"
+              id="inputPassword"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mb-3 form-control"
+              placeholder="Password"
+              required
+            />
+
+            <button className="btn btn-lg btn-primary btn-block" type="submit">
+              Sign in
+            </button>
+          </form>
         </div>
       </Container>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </>
   );
 }
