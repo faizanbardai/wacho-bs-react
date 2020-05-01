96664106548a7f15993cf7081709c696af0ab84e
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-05-02 00:58:59 +0200

    class to className

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 export default function PageNotFound() {
   return (
     <div id="notfound">
-      <div class="notfound">
-        <div class="notfound-404">
+      <div className="notfound">
+        <div className="notfound-404">
           <h1>Oops!</h1>
           <h2>404 - The Page can't be found</h2>
         </div>
