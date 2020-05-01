368e5072558dd3157c69251d7cd11bbf77dcf7d3
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-05-02 00:59:16 +0200

    formatting changes

--- src/actions.js
+++ src/actions.js
@@ -1,14 +1,14 @@
-export const increaseQty = _id => ({ type: "INCREASE_QTY", payload: _id });
-export const decreaseQty = _id => ({ type: "DECREASE_QTY", payload: _id });
-export const hideProductToast = _id => ({ type: "HIDE_PRODUCT_TOAST" });
+export const increaseQty = (_id) => ({ type: "INCREASE_QTY", payload: _id });
+export const decreaseQty = (_id) => ({ type: "DECREASE_QTY", payload: _id });
+export const hideProductToast = (_id) => ({ type: "HIDE_PRODUCT_TOAST" });
 export const loadProducts = () => {
-  return async dispatch => {
+  return async (dispatch) => {
     const baseURL = process.env.REACT_APP_BASE_URL;
-    const responseProducts = await fetch(baseURL + "/products");
+    const responseProducts = await fetch(baseURL + "/products?active=1");
     const products = await responseProducts.json();
     dispatch({
       type: "LOAD_PRODUCTS",
-      payload: products
+      payload: products,
     });
   };
 };
