bbedb384f844c98430189d0b6db92922125ec217
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-06-18 22:23:10 +0200

    Analytics code added

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactGA from "react-ga";
+import {
+  BrowserRouter as Router,
+  Route,
+  BrowserRouter,
+  Switch,
+} from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Admin from "./Pages/Admin/Admin";
+import Header from "./Components/Header";
+import contentInEnglish from "./en.json";
+import contentInGerman from "./de.json";
+import store from "./store";
+import { Provider } from "react-redux";
+import PageNotFound from "./Pages/PageNotFound/PageNotFound";
+
+function initializeReactGA() {
+  ReactGA.initialize("UA-123791717-1");
+  ReactGA.pageview("/homepage");
+}
+
+initializeReactGA();
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route path="/admin" component={Admin} />
+            <Route path="/en" exact>
+              <Home lang="en" content={contentInEnglish} />
+            </Route>
+            <Route path="/de" exact>
+              <Home lang="de" content={contentInGerman} />
+            </Route>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+            <Route component={PageNotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Router>
+    </Provider>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,43 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter as Router,
-  Route,
-  BrowserRouter,
-  Switch,
-} from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Admin from "./Pages/Admin/Admin";
-import Header from "./Components/Header";
-import contentInEnglish from "../src/en.json";
-import contentInGerman from "../src/de.json";
-import store from "./store";
-import { Provider } from "react-redux";
-import PageNotFound from "./Pages/PageNotFound/PageNotFound";
+import App from "./App.jsx";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      {false ? (
-        <div>Hi</div>
-      ) : (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <Switch>
-            <Route path="/admin" component={Admin} />
-            <Route path="/en" exact>
-              <Home lang="en" content={contentInEnglish} />
-            </Route>
-            <Route path="/de" exact>
-              <Home lang="de" content={contentInGerman} />
-            </Route>
-            <Route path="/" exact>
-              <Header />
-            </Route>
-            <Route component={PageNotFound} />
-          </Switch>
-        </BrowserRouter>
-      )}
-    </Router>
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
