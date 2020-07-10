4929e10d8339a3b508f9b105daefceea90937157
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-07-10 14:38:35 +0200

    shadow removed

--- src/Components/BannarText.jsx
+++ src/Components/BannarText.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BannarText(props) {
   return (
-    <div className="handwritten shadow-lg p-3 my-3 bg-white rounded text-center display-4">
+    <div className="handwritten p-3 my-3 bg-white rounded text-center">
       {props.text}
     </div>
   );
